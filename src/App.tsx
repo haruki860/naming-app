e49fd4877b41baecb804a821f599812041e7e7b0
有馬玄規 <haruki@arimagenkinoMacBook-Air.local>
2023-09-15 14:04:32 +0900

    環境構築と設定ファイルの追加

---
+++ src/App.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import Naming from './pages/Naming.tsx'
+
+const App:React.FC = () => {
+  return (
+    <Naming />
+  )
+}
+
+export default App
---
+++ src/main.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+
+ReactDOM.render(
+  <React.StrictMode>
+    <div className="bg-gray-100">
+      <App />
+    </div>
+  </React.StrictMode>,
+  document.getElementById("root")
+);
