1d97cbeb5a42731c15bd9a9daf49aa51d103b0aa
有馬玄規 <haruki@arimagenkinoMacBook-Air.local>
2023-10-02 11:19:58 +0900

    react-hook-formに書き換え

--- src/pages/Naming.tsx
+++ src/pages/Naming.tsx
@@ -1,46 +1,101 @@
-import React, { useState } from 'react'
+import { useForm, Controller } from 'react-hook-form'
+import React from 'react'
 import Dropdown from '../components/dropDown'
+
+type FormValues = {
+  language: string
+  kinds: string
+  export: string
+}
+
 const naming: React.FC = () => {
   const optionsLanguage = ['C', 'Java', 'Go']
   const optionsKinds = ['関数', '変数', '構造体', 'const', 'クラス']
   const optionsExport = ['有', '無']
-  const [selectedValue, setSelectedValue] = useState<string>('')
 
-  const handleDropdownChange = (selectedValue: string) => {
-    setSelectedValue(selectedValue)
+  const [selectedValues, setSelectedValues] = React.useState<FormValues>({
+    language: '',
+    kinds: '',
+    export: '',
+  })
+
+  const { control, handleSubmit } = useForm<FormValues>()
+
+  const onSubmit = (data: FormValues) => {
+    console.log(data)
   }
-
   return (
     <>
-      <h1 className="font-bold text-xl flex justify-center pt-8 mb-10">
+      <h1 className="font-bold text-xl flex justify-center  pt-8 mb-10">
         作成したいクラスの条件を入力してください
       </h1>
-      <div className="flex justify-center gap-x-32">
-        <div>
-          <h2 className="text-xl">言語</h2>
-          <Dropdown
-            options={optionsLanguage}
-            value={selectedValue}
-            onChange={handleDropdownChange}
-          />
+      <form onSubmit={handleSubmit(onSubmit)}>
+        <div className="flex justify-center flex-nowrap gap-x-32">
+          <div className="flex">
+            <h2 className="text-xl">言語</h2>
+            <Controller
+              name="language"
+              control={control}
+              defaultValue=""
+              render={({ field }) => (
+                <Dropdown
+                  options={optionsLanguage}
+                  value={selectedValues.language}
+                  onChange={(selectedValue) => {
+                    field.onChange(selectedValue)
+                    setSelectedValues({
+                      ...selectedValues,
+                      language: selectedValue,
+                    })
+                  }}
+                />
+              )}
+            />
+          </div>
+          <div className="flex">
+            <h3 className="text-xl">種類</h3>
+            <Controller
+              name="kinds"
+              control={control}
+              defaultValue=""
+              render={({ field }) => (
+                <Dropdown
+                  options={optionsKinds}
+                  value={selectedValues.kinds}
+                  onChange={(selectedValue) => {
+                    field.onChange(selectedValue)
+                    setSelectedValues({
+                      ...selectedValues,
+                      kinds: selectedValue,
+                    })
+                  }}
+                />
+              )}
+            />
+          </div>
+          <div className="flex">
+            <h4 className="text-xl">export</h4>
+            <Controller
+              name="export"
+              control={control}
+              defaultValue=""
+              render={({ field }) => (
+                <Dropdown
+                  options={optionsExport}
+                  value={selectedValues.export}
+                  onChange={(selectedValue) => {
+                    field.onChange(selectedValue)
+                    setSelectedValues({
+                      ...selectedValues,
+                      export: selectedValue,
+                    })
+                  }}
+                />
+              )}
+            />
+          </div>
         </div>
-        <div>
-          <h3 className="text-xl">種類</h3>
-          <Dropdown
-            options={optionsKinds}
-            value={selectedValue}
-            onChange={handleDropdownChange}
-          />
-        </div>
-        <div>
-          <h4 className="text-xl">export</h4>
-          <Dropdown
-            options={optionsExport}
-            value={selectedValue}
-            onChange={handleDropdownChange}
-          />
-        </div>
-      </div>
+      </form>
     </>
   )
 }
